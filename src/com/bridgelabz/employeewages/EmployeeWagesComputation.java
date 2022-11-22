0b1f0c48d5e24a8bcd69a688862f6c22927e07ea
shiwani <shiwanipaunikar1@gmail.com>
2022-11-21 21:22:54 -0800

    [shiwani]employee wages computation program added

---
+++ src/com/bridgelabz/employeewages/EmployeeWagesComputation.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeewages;
+
+public class EmployeeWagesComputation {
+    public static void main(String[] args) {
+        System.out.println("welcome to employee wages program");
+    }
+}
+
